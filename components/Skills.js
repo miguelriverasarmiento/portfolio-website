31d2c53208a32466f88a0f0c2b421caa56cb4feb
miguelriverasarmiento <miguelriverasarmiento@gmail.com>
2023-05-11 17:30:40 -0500

    Added skill level

--- components/Skills.js
+++ components/Skills.js
@@ -10,52 +10,84 @@ const Skills = () => {
 
         <div className="grid grid-cols-1 justify-items-center sm:grid-cols-2 md:grid-cols-3 gap-5">
 
-            <div className="shadow-lg flex flex-col items-center w-[150px] h-[200px] gap-4 px-8 py-10 
+            <div className="shadow-lg flex flex-col items-center w-[150px] h-[200px] gap-4 px-8 pt-6 
             bg-neutral-500 text-green-300">
             <Image width={300} height={300} className="w-[150px]" src="/img/js.png" alt=""/>
                 Javascript
+                <div className="grid grid-cols-3 place-items-center text-gray-700 text-[12px] font-medium">
+                    <div className='bg-gray-700 h-[4px] w-[28px] col-span-2'></div>
+                    <div>&nbsp;80%</div>
+                </div>
             </div>
 
-            <div className="shadow-lg flex flex-col items-center w-[150px] h-[200px] gap-4 px-8 py-10 
+            <div className="shadow-lg flex flex-col items-center w-[150px] h-[200px] gap-4 px-8 pt-6  
             bg-neutral-500 text-green-300">
             <Image width={300} height={300} className="w-[150px]" src="/img/react.png" alt=""/>
                 React
+                <div className="grid grid-cols-3 place-items-center text-gray-700 text-[12px] font-medium">
+                    <div className='bg-gray-700 h-[4px] w-[35px] col-span-2'></div>
+                    <div>&nbsp;90%</div>
+                </div>
             </div>
 
-            <div className="shadow-lg flex flex-col items-center w-[150px] h-[200px] gap-4 px-8 py-10 
+            <div className="shadow-lg flex flex-col items-center w-[150px] h-[200px] gap-4 px-8 pt-6 
             bg-neutral-500 text-green-300">
             <Image width={300} height={300} className="w-[150px]" src="/img/typescript.png" alt=""/>
                 Typescript
+                <div className="grid grid-cols-3 place-items-center text-gray-700 text-[12px] font-medium">
+                    <div className='bg-gray-700 h-[4px] w-[24px] col-span-2'></div>
+                    <div>&nbsp;60%</div>
+                </div>
             </div>
 
-            <div className="shadow-lg flex flex-col items-center w-[150px] h-[200px] gap-4 px-8 py-10 
+            <div className="shadow-lg flex flex-col items-center w-[150px] h-[200px] gap-4 px-8 pt-6 
             bg-neutral-500 text-green-300">
             <Image width={300} height={300} className="w-[150px]" src="/img/html.png" alt=""/>
                 Html
+                <div className="grid grid-cols-3 place-items-center text-gray-700 text-[12px] font-medium">
+                    <div className='bg-gray-700 h-[4px] w-[35px] col-span-2'></div>
+                    <div>&nbsp;90%</div>
+                </div>
             </div>
 
-            <div className="shadow-lg flex flex-col items-center w-[150px] h-[200px] gap-4 px-8 py-10 
+            <div className="shadow-lg flex flex-col items-center w-[150px] h-[200px] gap-4 px-8 pt-6 
             bg-neutral-500 text-green-300">
             <Image width={300} height={300} className="w-[150px]" src="/img/css.png" alt=""/>
                 Css
+                <div className="grid grid-cols-3 place-items-center text-gray-700 text-[12px] font-medium">
+                    <div className='bg-gray-700 h-[4px] w-[35px] col-span-2'></div>
+                    <div>&nbsp;90%</div>
+                </div>
             </div>
 
-            <div className="shadow-lg flex flex-col items-center w-[150px] h-[200px] gap-4 px-8 py-10 
+            <div className="shadow-lg flex flex-col items-center w-[150px] h-[200px] gap-4 px-8 pt-6 
             bg-neutral-500 text-green-300">
             <Image width={300} height={300} className="w-[150px]" src="/img/nextjs-icon.png" alt=""/>
                 Next.js
+                <div className="grid grid-cols-3 place-items-center text-gray-700 text-[12px] font-medium">
+                    <div className='bg-gray-700 h-[4px] w-[24px] col-span-2'></div>
+                    <div>&nbsp;60%</div>
+                </div>
             </div>
 
-            <div className="shadow-lg flex flex-col items-center w-[150px] h-[200px] gap-4 px-8 py-10 
+            <div className="shadow-lg flex flex-col items-center w-[150px] h-[200px] gap-4 px-8 pt-4 
             bg-neutral-500 text-green-300">
             <Image width={300} height={300} className="w-[150px]" src="/img/graphql.png" alt=""/>
                 GraphQL
+                <div className="grid grid-cols-3 place-items-center text-gray-700 text-[12px] font-medium">
+                    <div className='bg-gray-700 h-[4px] w-[24px] col-span-2'></div>
+                    <div>&nbsp;60%</div>
+                </div>
             </div>
 
-            <div className="shadow-lg flex flex-col items-center w-[150px] h-[200px] gap-4 px-5 py-6 
+            <div className="shadow-lg flex flex-col items-center w-[150px] h-[200px] gap-4 px-5 pt-0 
             bg-neutral-500 text-green-300">
             <Image width={300} height={300} className="w-[150px] mx-0 my-0" src="/img/tailwindcss.png" alt=""/>
                 Tailwind CSS
+                <div className="grid grid-cols-3 place-items-center text-gray-700 text-[12px] font-medium">
+                    <div className='bg-gray-700 h-[4px] w-[28px] col-span-2'></div>
+                    <div>&nbsp;80%</div>
+                </div>
             </div>
 
         </div>

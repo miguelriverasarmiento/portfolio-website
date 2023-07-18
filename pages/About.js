66e5f5954f88d38e46db2d8261c0c71c2b49a349
miguelriverasarmiento <miguelriverasarmiento@gmail.com>
2023-07-18 10:23:30 -0500

    Added new photo 2023

--- pages/About.js
+++ pages/About.js
@@ -18,7 +18,7 @@ const About = () => {
         <div className='justify-self-center w-[250px]'>
           <Image 
             className="border-slate-400" 
-            src="/img/miguelRiv2023.png" 
+            src="/img/miguelriv23.png" 
             width={400} 
             height={400}
             alt=""/>

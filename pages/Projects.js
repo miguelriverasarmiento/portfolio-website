f8d910732312445c320bb7376b6a865266f6a5f5
miguelriverasarmiento <miguelriverasarmiento@gmail.com>
2023-08-09 09:58:22 -0500

    added new project

--- pages/Projects.js
+++ pages/Projects.js
@@ -15,31 +15,30 @@ const Projects = () => {
 
             <div className="flex flex-col items-center relative group">
                 <Image className="w-[100%] h-[100%] shadow-2xl"  width={300} 
-                height={300} src='/img/premier-app.png' alt=''/>
+                height={300} src='/img/online-store.png' alt=''/>
                 <div className="bg-[#000000bd] absolute w-[100%] h-[100%]
                 top-0 opacity-0 transition duraction-500 group-hover:opacity-100
-                grid place-items-center text-purple-600 text-[12px] sm:text-[15px] 
+                grid place-items-center text-yellow-600 text-[12px] sm:text-[15px] 
                 md:text-[17px]">
-                    Pagina de la Premier League
+                    Pagina de tienda online
                 </div>
             </div>
 
             <div className='flex flex-col text-left w-[80%] px-3 py-3 text-[12px] sm:text-[15px] font-mono 
             shadow-2xl shadow hover:shadow-xl text-teal-950'>
-                Este proyecto es una pagina web de informacion
-                acerca de la premier league, contiene equipos,
-                jugadores, tabla de posiciones, goleadores, 
-                asistidores y el calendario de partidos.
-                Esta hecho con react, javascript, html, css.
+                Este proyecto es una pagina web de una tienda
+                online de instrumentos musicales. Utiliza el hook
+                useContext para manejar metodos y variables globalmente
+                Esta hecho con react, javascript, html y tailwind.css.
 
                 <div className='flex flex-col text-left mt-2 rounded shadow-sm'>
                     <div className="flex flex-row">
                         <div><TbLink className='text-red-800 text-[22px] pr-1'/></div>
-                        <div><Link href="https://mg-ars-pl.vercel.app/">Ir a mg-ars-pl</Link></div>
+                        <div><Link href="https://mgars-store.vercel.app/">Ir a mg-ars store</Link></div>
                     </div>
                     <div className="flex flex-row">
                         <div><RiGitRepositoryLine className='text-red-800 text-[22px] pr-1'/></div>
-                        <div><Link href="https://github.com/miguelriverasarmiento/app-soccer">Ir a repositorio</Link></div>
+                        <div><Link href="https://github.com/miguelriverasarmiento/tienda-online">Ir a repositorio</Link></div>
                     </div>
                 </div>
             </div>
